a2006fe47cf40a3950af5c9c12e1bfb4b9b20056
koooyooo <jab.cross.hook@gmail.com>
2019-09-16 02:44:14 +0900

    enhance testcase

--- errors/errors_test.go
+++ errors/errors_test.go
@@ -46,6 +46,7 @@ func TestNew(t *testing.T) {
 	e := New(L(), "new error")
 	assert.Equal(t, "new error", e.Error())
 	assert.Equal(t,"new error", e.Msg)
+	assert.Nil(t, e.Cause())
 	assert.True(t, strings.HasSuffix(e.Stack.FuncName, "TestNew"))
 
 	// runtime.goexit & testing.tRunner & TestNew => 3
@@ -58,6 +59,7 @@ func TestErrorf(t *testing.T) {
 	e := Errorf(L(), "new %s", "error")
 	assert.Equal(t, "new error", e.Error())
 	assert.Equal(t, "new error", e.Msg)
+	assert.Nil(t, e.Cause())
 	assert.True(t, strings.HasSuffix(e.Stack.FuncName, "TestErrorf"))
 
 	// runtime.goexit & testing.tRunner & TestNew => 3
@@ -71,7 +73,13 @@ func TestWrap(t *testing.T) {
 	e := Wrap(NoLabel, "doing xxx errors", cause)
 	assert.Equal(t, "doing xxx errors || original", e.Error())
 	assert.Equal(t, "doing xxx errors", e.Msg)
+	assert.Equal(t, cause, e.Cause())
 	assert.True(t, strings.HasSuffix(e.Stack.FuncName, "TestWrap"))
+
+	// runtime.goexit & testing.tRunner & TestNew => 3
+	len := len(e.RawStackTraces)
+	assert.Equal(t, len, 3)
+	assert.True(t, strings.HasSuffix(e.RawStackTraces[len-1].FuncName, "TestWrap"))
 }
 
 func TestErrorMethod(t *testing.T) {
@@ -80,20 +88,48 @@ func TestErrorMethod(t *testing.T) {
 	assert.Equal(t, `fail in calling f1() || fail in calling f2() || API call failed || {"Msg":"Auth API call","URL":"https://test.auth.com","Method":"Get","StCode":500}`, error.Error())
 }
 
-func TestDo(t *testing.T) {
+func TestStackTraceString(t *testing.T) {
 	err := f0()
-	error, ok := err.(*Error)
+	error, _ := err.(*Error)
+	s := error.StackTraceString()
+	// contains FuncNames
+	assert.Contains(t, s, "errors.f0")
+	assert.Contains(t, s, "errors.f1")
+	assert.Contains(t, s, "errors.f2")
+	// contains Msgs
+	assert.Contains(t, s, "fail in calling f1()")
+	assert.Contains(t, s, "fail in calling f2()")
+	assert.Contains(t, s, "API call failed")
+}
+
+func TestRawStackTraceString(t *testing.T) {
+	err := f0()
+	error, _ := err.(*Error)
+	s := error.RawStackTraceString()
+	// contains FuncNames
+	assert.Contains(t, s, "errors.f0")
+	assert.Contains(t, s, "errors.f1")
+	assert.Contains(t, s, "errors.f2")
+}
+
+func TestCheckOutput(t *testing.T) {
+	err := f0()
+	error, _ := err.(*Error)
+
+	// Output Msg
 	fmt.Println(err)
 	fmt.Println()
-	if !ok {
-		fmt.Println(err.Error())
-	} else {
-		error.PrintStackTrace(os.Stdout)
-		fmt.Println()
-		error.PrintRawStackTrace(os.Stdout)
-		fmt.Println()
-		if api, ok := error.Cause().(ApiCallError); ok {
-			fmt.Printf("Operate API Call Error: %v", api)
-		}
+
+	// Output StackTrace
+	error.PrintStackTrace(os.Stdout)
+	fmt.Println()
+
+	// Output RawStackTrace
+	error.PrintRawStackTrace(os.Stdout)
+	fmt.Println()
+
+	// Check Cause
+	if apiErr, ok := error.Cause().(ApiCallError); ok {
+		fmt.Println(apiErr)
 	}
 }
